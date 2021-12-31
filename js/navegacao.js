ec7ba65552d04290fe307b72caecc4751001b4b8
André Luis Kunert-ProgramadorWebJavaScript <91467055+ProgramadorWebJavascriprt@users.noreply.github.com>
2021-12-31 14:17:18 -0300

    Curso Web Moderno Completo

---
+++ js/navegacao.js
@@ -0,0 +1,43 @@
+(function () {
+    function navegarViaAjax(hash) {
+        if (!hash) return
+
+        const link = document.querySelector(`[wm-link='${hash}']`)
+        if(!link) return
+
+        const destino = document.querySelector('[wm-link-destino]')
+
+        const url = hash.substring(1)
+        fetch(url)
+            .then(resp => resp.text())
+            .then(html => {
+                destino.innerHTML = html
+                const resutado = html.match(/\<script>([\s\S]*)\<\script\>/)
+                if (resutado && resultado.length >= 2){  
+                    eval(resutado[1]) 
+                }
+                
+            })
+    }
+
+    function configurarLinks() {
+        document.querySelectorAll('[wm-link]')
+            .forEach(link => {
+                link.href = link.attributes['wm-link'].value
+            })
+    }
+
+    function navegacaoInicial() {
+        if (location.hash) {
+            navegarViaAjax(location.hash)
+        } else {
+            const primeiroLink = document.querySelector('[wm-link]')
+            navegarViaAjax(primeiroLink.hash)
+        }
+    }
+
+    window.onhashchange = e => navegarViaAjax(location.hash)
+    
+    configurarLinks()
+    navegacaoInicial()
+})()
